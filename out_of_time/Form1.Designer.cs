6144d79fbe03459fb02794da8a5e453ba5c216ac
qhgongzi <qhgongzi@gmail.com>
2013-09-08 17:49:30 +0800

    去掉 最小化，最大化按钮

--- out_of_time/Form1.Designer.cs
+++ out_of_time/Form1.Designer.cs
@@ -142,6 +142,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.tb_task_title);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
